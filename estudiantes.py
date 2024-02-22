ba23fcac92ad6050070754bd1d08cbd4deb4592e
j41savage <160883331+j41savage@users.noreply.github.com>
2024-02-22 11:15:46 -0500

    PROGRAMACION CLASE

---
+++ estudiantes.py
@@ -0,0 +1,16 @@
+
+
+
+class estudiantes:
+    def __init__(self, firstname="Nombre", lastname="Apellido", mail="Correo", phone="Telefono"):
+        
+     self.firstname = firstname
+     self.lastname = lastname
+     self.mail = mail
+     self.phone = phone
+    
+    def obtenerinfo(self):
+        print("Mi nombre es",self.firstname,self.lastname)
+       
+
+       
---
+++ main.py
@@ -0,0 +1,4 @@
+from estudiantes import estudiantes
+
+p=estudiantes("JAIME", "CARREAZO", "jaimenriquecarreazo@gmail.com","3246098991")
+print(p.obtenerinfo())
